0e282ddd66aa8c013323cfa0e6f86921005272c4
Lars Frölich <lars.froelich@gmail.com>
2025-12-11 17:15:12 +0100

    gw: hotfix ota-update with missing sw_title attribute

--- software/gateway/src/on_mqtt_msg/check_for_ota_updates.py
+++ software/gateway/src/on_mqtt_msg/check_for_ota_updates.py
@@ -16,13 +16,13 @@ def on_msg_check_for_ota_update(msg_payload: Optional[Any]) -> bool:
     if docker_client.is_controller_running():
         current_version = docker_client.get_controller_version()
         if current_version is None or current_version != sw_version:
-            info("Software update available: " + sw_title + " from " + (
+            info("Software update available: " + (sw_title or "?") + " from " + (
                         current_version or "UNKNOWN") + " to " + sw_version)
             docker_client.start_controller_safely(sw_version)
         else:
             info("Software is up to date (version '" + current_version + "')")
             docker_client.set_last_launched_controller_version(current_version)
     else:
-        info("Launching latest edge-software: " + sw_version + " (" + sw_title + ")")
+        info("Launching latest edge-software: " + sw_version + " (" + (sw_title or "?") + ")")
         docker_client.start_controller_safely(sw_version)
     return True
